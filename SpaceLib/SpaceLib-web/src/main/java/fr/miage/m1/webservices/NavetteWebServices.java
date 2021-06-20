fe64d1d6cfe26960407c5924648ca89e8d9c6f87
DamienLinux <damien.avetta@gmail.com>
2021-06-20 14:56:19 +0200

    Annule, carte et reservation webservices

--- SpaceLib/SpaceLib-web/src/main/java/fr/miage/m1/webservices/NavetteWebServices.java
+++ SpaceLib/SpaceLib-web/src/main/java/fr/miage/m1/webservices/NavetteWebServices.java
@@ -6,14 +6,21 @@
 package fr.miage.m1.webservices;
 
 import fr.miage.m1.server.services.ServicesNavetteLocal;
+import fr.miage.m1.shared.exceptions.AucuneDestinationException;
+import fr.miage.m1.shared.exceptions.DestinationIncorrecteException;
+import fr.miage.m1.shared.exceptions.IdReservationIncorrecteException;
+import fr.miage.m1.shared.exceptions.MauvaisUtilisateurReservationException;
 import fr.miage.m1.shared.exceptions.NavetteExistanteException;
 import fr.miage.m1.shared.exceptions.NavetteInexistanteException;
 import fr.miage.m1.shared.exceptions.NavettePasAReviserException;
+import fr.miage.m1.shared.exceptions.NavettePassagersException;
+import fr.miage.m1.shared.exceptions.NavettesIndisponibleException;
 import fr.miage.m1.shared.exceptions.QuaiIndisponibleException;
 import fr.miage.m1.shared.exceptions.RevisionInexistanteException;
 import fr.miage.m1.shared.exceptions.RoleInvalideException;
 import fr.miage.m1.shared.exceptions.StationInexistanteException;
 import fr.miage.m1.shared.exceptions.TokenInvalideException;
+import java.text.ParseException;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.jws.WebMethod;
@@ -83,4 +90,32 @@ public class NavetteWebServices {
         ejbRef.ajouterNavette(infosCompte, navette, nbPassagersInt, station);
     }
     
+    @WebMethod(operationName = "reserve")
+    public Long reserve(@WebParam(name = "identifiant") String identifiant, @WebParam(name = "token") String token, @WebParam(name = "stationAttachement") String stationAttachement, 
+                        @WebParam(name = "destination") String destination, @WebParam(name = "dateDepart") String dateDepart, 
+                        @WebParam(name = "nbPassagers") int nbPassagers) 
+                throws TokenInvalideException, AucuneDestinationException, 
+                       QuaiIndisponibleException, StationInexistanteException, 
+                       NavettesIndisponibleException, ParseException, 
+                       DestinationIncorrecteException, NavettePassagersException {
+        String[] infosCompte;
+        
+        infosCompte = new String[2];
+        infosCompte[0] = identifiant;
+        infosCompte[1] = token;
+        return ejbRef.reserve(infosCompte, stationAttachement, destination, dateDepart, nbPassagers);
+    }
+
+    @WebMethod(operationName = "annule")
+    public void annule(@WebParam(name = "identifiant") String identifiant, @WebParam(name = "token") String token, 
+                       @WebParam(name = "idReservation") String idReservation) 
+                throws TokenInvalideException, IdReservationIncorrecteException, 
+                       MauvaisUtilisateurReservationException {
+        String[] infosCompte;
+        
+        infosCompte = new String[2];
+        infosCompte[0] = identifiant;
+        infosCompte[1] = token;
+        ejbRef.annule(infosCompte, idReservation);
+    }
 }
